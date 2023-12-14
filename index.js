30d90e78bdec6e46a554807b885092310b239962
Shabeer <shabeerwebdev@gmail.com>
2023-12-14 21:12:09 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Listen for incoming messages
+  socket.on('chat message', (msg) => {
+    // Broadcast the message to all connected clients
+    io.emit('chat message', msg);
+  });
+
+  // Listen for user disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
